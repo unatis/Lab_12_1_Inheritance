4f5dbd19d065d9c13d2487bf24f558c3ccf5a05b
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-05 16:12:11 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car Mercedes = new Car();
+
+        Mercedes.Honk();
+        Mercedes.BrandName = "Mercedes";
+        Mercedes.CarModel = "SLK300";
+        Mercedes.Drive();
+    }
+}
