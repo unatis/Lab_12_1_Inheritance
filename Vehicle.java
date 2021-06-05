f2d78f121c2d28796301c3cdd9b361ad08a7d0a3
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-05 16:12:56 +0200

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Vehicle {
+
+    public String BrandName = "";
+    public String CarType = "";
+    String MotorType = "";
+
+    public void Honk() {
+        System.out.println("Tuut, tuut!");
+    }
+
+    public void Drive() {
+        System.out.println("Go forward");
+    }
+}
