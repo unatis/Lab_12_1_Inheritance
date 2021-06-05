64553f23573d25b2cd28d2449fca2cb9f21329d2
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-05 16:12:32 +0200

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Car extends Vehicle{
+
+    String CarModel = "";
+
+    @Override
+    public void Drive() {
+        System.out.println("Go backward");
+    }
+
+}
