f422e2cfb19e2d659d681c9c00334673931d612c
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-07 19:05:59 +0200

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -3,14 +3,10 @@ package com.company;
 public class Vehicle {
 
     public String BrandName = "";
-    public String CarType = "";
-    String MotorType = "";
+    public String Type = "";
 
-    public void Honk() {
-        System.out.println("Tuut, tuut!");
-    }
-
-    public void Drive() {
+    public void Drive (){
         System.out.println("Go forward");
     }
+
 }
